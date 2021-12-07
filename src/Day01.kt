8eede63e34af8839f179c80d4e9eb407afa07b3e
Rachel Arthur <arthur.rachel@heb.com>
2021-12-07 13:48:03 -0600

    day 01 both parts

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,35 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+
+        // original solution
+
+//        var result = 0
+//        var oldDepth = 0
+//        input.forEachIndexed { index, newDepth ->
+//            if (index > 0) {
+//                if (oldDepth < newDepth) result += 1
+//            }
+//            oldDepth = newDepth
+//        }
+
+        // idiomatic solution
+        return input.windowed(2).count() {
+            (a, b) -> a < b
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        return input.windowed(3).windowed(2).count() {
+            (a, b) -> a.sum() < b.sum()
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01_input").map { it.toInt() }
+
     println(part1(input))
     println(part2(input))
 }
